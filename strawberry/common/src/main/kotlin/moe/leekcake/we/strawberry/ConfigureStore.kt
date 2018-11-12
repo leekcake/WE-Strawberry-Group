cc911ffb8d4d127cca6acdb45a744b1427c2c131
Blue Electric <fkdlxmslrx@gmail.com>
2018-11-12 23:22:52 +0900

    Add background header

--- strawberry/common/src/main/kotlin/moe/leekcake/we/strawberry/ConfigureStore.kt
+++ strawberry/common/src/main/kotlin/moe/leekcake/we/strawberry/ConfigureStore.kt
@@ -3,6 +3,7 @@ package moe.leekcake.we.strawberry
 import moe.leekcake.we.basekit.*
 
 object ConfigureStore {
+    val background = EmptyConfigure("background", "ui_background")
     val backgroundFile = FileConfigure("backgroundFile", "ui_backgroundFile")
     val backgroundColor = ColorConfigure("backgroundColor", "ui_backgroundColor", 255, 209, 220)
 }
